223f7657b28bf7632706d50ed7e795a063708e6d
dariansweb <dariansweb@gmail.com>
2025-01-23 10:17:28 -0600

    ratings and average review functional

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FeedbackStats = ({ feedback }) => {
+  // Calculate ratings average
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4> {feedback.length} Reviews </h4>
+      <h4>
+        {" "}
+        Average Rating:{" "}
+        {isNaN(average) ? 0 : average.toFixed(1).replace(/\.0$/, "")}
+      </h4>
+    </div>
+  );
+};
+
+export default FeedbackStats;
